a8bbd572f59a01579ca7ee6eca2a42d9181060de
Jnanashish <jhandique1999@gmail.com>
2023-05-14 19:15:52 +0530

    infinite scrolling done

--- src/Core/apiclient.js
+++ src/Core/apiclient.js
@@ -1,29 +1,29 @@
 const url = "https://api.flickr.com/services/rest";
 const apiKey = "a6646865e3024740c83fe03d80322fc7";
 
-export const getImages = async () => {
+export const getImages = async (pageNo) => {
     const params = new URLSearchParams({
         method: "flickr.photos.getRecent",
         api_key: apiKey,
         format: "json",
         nojsoncallback: 1,
-        per_page: 1,
-        page: 1,
+        per_page: 10,
+        page: pageNo,
     });
 
     const apiResponse = await fetch(`${url}?${params}`);
     return apiResponse;
 };
 
-export const getSearchedImage = async (searchedTerm) => {
+export const getSearchedImage = async (searchedTerm, pageNo) => {
     const params = new URLSearchParams({
         method: "flickr.photos.search",
         api_key: apiKey,
         text: searchedTerm,
         format: "json",
         nojsoncallback: 1,
-        per_page: 1,
-        page: 1,
+        per_page: 10,
+        page: pageNo,
     });
     const apiResponse = await fetch(`${url}?${params}`);
     return apiResponse;
--- src/components/Dashboard/index.jsx
+++ src/components/Dashboard/index.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useState } from "react";
 import Gallery from "../Gallery";
 import { getImages, getSearchedImage } from "../../Core/apiclient.js";
@@ -5,70 +6,88 @@ import styles from "./dashnoard.module.css";
 import _ from "lodash";
 
 function Dashboard() {
-    const [imageData, setImageData] = useState(null);
+    const [imageData, setImageData] = useState([]);
     const [searchTerm, setSearchTerm] = useState("");
     const [apiError, setApiError] = useState(false);
     const [notFound, setNotFound] = useState(false);
     const [showLoader, setShowLoader] = useState(false);
     const [searchQueries, setSearchQueries] = useState([]);
+    const [pageNo, setPageNo] = useState(1);
 
+    // onintial load call the image list api
     useEffect(() => {
         getImageList();
     }, []);
 
-    useEffect(() => {
-        const savedQueries = JSON.parse(localStorage.getItem("searchQueries")) || [];
-        setSearchQueries(savedQueries);
-    }, []);
-    useEffect(() => {
-        localStorage.setItem("searchQueries", JSON.stringify(searchQueries));
-    }, [searchQueries]);
-
     // implemented debouce to make api call on searched term
     const delayedSearch = _.debounce(() => {
-        if (searchTerm.length > 0) loadSearchedImage();
+        loadSearchedImage();
     }, 800);
 
     useEffect(() => {
-        delayedSearch(searchTerm);
+        if (searchTerm.length > 0) {
+            setImageData([]);
+            delayedSearch(searchTerm);
+        }
         return delayedSearch.cancel;
     }, [searchTerm]);
 
-    const loadSearchedImage = () => {
-        if (!searchTerm.trim()) {
-            return;
+    // handle infinite scroll
+    window.onscroll = () => {
+        if (
+            !showLoader &&
+            window.innerHeight + document.documentElement.scrollTop + 200 >
+                document.documentElement.offsetHeight
+        ) {
+            setPageNo((prevstate) => prevstate + 1);
+            if (searchTerm.length > 1) loadSearchedImage(pageNo + 1);
+            else getImageList(pageNo + 1);
         }
-        setSearchQueries([...searchQueries, searchTerm.trim()]);
-
-        setShowLoader(true);
-        setImageData(null);
-        setNotFound(false);
-        getSearchedImage(searchTerm)
-            .then((response) => {
-                setShowLoader(false);
-                setSearchTerm("");
-                return response.json();
-            })
-            .then((data) => {
-                if (data.photos.photo.length === 0) setNotFound(true);
-                return setImageData(data.photos.photo);
-            })
-            .catch((error) => setApiError(true));
     };
 
     const handleQuerySelect = (query) => {
         setSearchTerm(query);
     };
 
-    // make api call to get list of images on intial load
-    const getImageList = () => {
-        getImages()
+    // make api call to load image with searched query
+    const loadSearchedImage = (pageNo) => {
+        if (!searchTerm.trim()) {
+            return;
+        }
+        setSearchQueries([...searchQueries, searchTerm.trim()]);
+        setShowLoader(true);
+        setNotFound(false);
+        getSearchedImage(searchTerm, pageNo)
             .then((response) => {
-                setShowLoader(false);
                 return response.json();
             })
-            .then((data) => setImageData(data.photos.photo))
+            .then((data) => {
+                setShowLoader(false);
+                if (data.photos.photo.length === 0) setNotFound(true);
+                return setImageData([...imageData, ...data.photos.photo]);
+            })
             .catch((error) => setApiError(true));
+    };
+
+    // make api call to get list of images on intial load
+    const getImageList = (pageNo) => {
+        setShowLoader(true);
+        getImages(pageNo)
+            .then((response) => {
+                return response.json();
+            })
+            .then((data) => {
+                setShowLoader(false);
+                setImageData([...imageData, ...data.photos.photo]);
+            })
+            .catch((error) => setApiError(true));
+    };
+
+    const clearSearchKeyword = () => {
+        setSearchTerm("");
+        setImageData([]);
+        setPageNo(1);
+        getImageList(1);
     };
 
     return (
@@ -76,13 +95,18 @@ function Dashboard() {
             {/* header with search container  */}
             <div className={styles.seach_container}>
                 <p>Search Photos</p>
-                <input
-                    value={searchTerm}
-                    onChange={(e) => setSearchTerm(e.target.value)}
-                    className={styles.input_container}
-                    placeholder="Enter any keyword"
-                />
-                {searchQueries.length > 0 && (
+                <div className={styles.input_con}>
+                    <input
+                        value={searchTerm}
+                        onChange={(e) => setSearchTerm(e.target.value)}
+                        className={styles.input_container}
+                        placeholder="Enter any keyword"
+                    />
+                    <p onClick={() => clearSearchKeyword()} className={styles.cancelButton}>
+                        {searchTerm.length > 1 ? "x" : ""}
+                    </p>
+                </div>
+                {searchQueries.length > 0 && false && (
                     <div className={styles.search_suggestions}>
                         {searchQueries.map((query, index) => (
                             <p
@@ -96,7 +120,7 @@ function Dashboard() {
                 )}
             </div>
 
-            <Gallery data={imageData} />
+            {imageData.length > 0 && <Gallery data={imageData} />}
 
             {/* error block to show error or not found message  */}
             {(apiError || notFound) && (
@@ -105,7 +129,9 @@ function Dashboard() {
                 </div>
             )}
 
-            {showLoader && <div className={styles.error_container}>Loading ...</div>}
+            {(showLoader || imageData.length === 0) && (
+                <div className={styles.error_container}>Loading ...</div>
+            )}
         </div>
     );
 }
